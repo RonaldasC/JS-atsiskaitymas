f13b410a995016c785291e05e42037333a308eb3
RonaldasC <ronaldasr@gmail.com>
2022-06-25 19:24:48 +0300

    formatavimas js

--- Task 3/script.js
+++ Task 3/script.js
@@ -12,41 +12,41 @@ turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 "use strict";
 
-const ENDPOINT = 'https://api.github.com/users';
+const ENDPOINT = "https://api.github.com/users";
 
 async function getResponse(arg) {
-    const response = await fetch(arg);
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const data = await response.json();
-    returnUserAvatar(data);
+  const response = await fetch(arg);
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
   }
+  const data = await response.json();
+  returnUserAvatar(data);
+}
 
-  function returnUserAvatar(arg) {
-    for (let i = 0; i < arg.length; i++) {
-      let user = arg[i].login;
-      let avatar = arg[i].avatar_url;
-      const box = document.getElementById("output");
-      const cardForDisplayBrand = document.createElement("div");
-      const cardForDisplayModel = document.createElement("div");
-      cardForDisplayBrand.classList.add("brand-card");
-      cardForDisplayModel.classList.add("model-card");
-      cardForDisplayBrand.innerHTML = `<H2>${user}</H2>`;
-      cardForDisplayModel.innerHTML = `<img src="${avatar}" alt="face" height="80px" width="70px"></img>`;
-      box.append(cardForDisplayBrand);
-      cardForDisplayBrand.append(cardForDisplayModel);
-    }
+function returnUserAvatar(arg) {
+  for (let i = 0; i < arg.length; i++) {
+    let user = arg[i].login;
+    let avatar = arg[i].avatar_url;
+    const box = document.getElementById("output");
+    const cardForDisplayBrand = document.createElement("div");
+    const cardForDisplayModel = document.createElement("div");
+    cardForDisplayBrand.classList.add("brand-card");
+    cardForDisplayModel.classList.add("model-card");
+    cardForDisplayBrand.innerHTML = `<H2>${user}</H2>`;
+    cardForDisplayModel.innerHTML = `<img src="${avatar}" alt="face" height="80px" width="70px"></img>`;
+    box.append(cardForDisplayBrand);
+    cardForDisplayBrand.append(cardForDisplayModel);
   }
+}
 
-  function removeElement() {
-    const element = document.getElementById("message");
-    element.remove();
-  }
+function removeElement() {
+  const element = document.getElementById("message");
+  element.remove();
+}
 
-  function displayUsers() {
-    removeElement();
-    getResponse(ENDPOINT);
-  }
+function displayUsers() {
+  removeElement();
+  getResponse(ENDPOINT);
+}
 
-  document.getElementById("btn").addEventListener("click", displayUsers);
+document.getElementById("btn").addEventListener("click", displayUsers);
